fc95f557641d2594e776d84c369df10aebbfda09
samuel <00023020@uca.edu.sv>
2025-09-22 21:27:50 -0600

    :sparkles: Project update

--- index.js
+++ index.js
@@ -1,78 +1,72 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const dns = require('node:dns')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const dns = require("node:dns");
+const { MongoClient } = require("mongodb");
 
-// Basic Configuration
+// Express configuration
+const app = express();
 const port = process.env.PORT || 3000;
 
-let urls = {}
+// Mongo DB configuration
+const client = new MongoClient(process.env.DB_URL);
+const database = client.db("urlshortener");
+var urls = database.collection("urls");
 
-function checkUrl(url) {
-  try{
-    let parsedUrl = new URL(url)
-    if(parsedUrl.protocol==='http:' || parsedUrl.protocol==='https:'){
-      return true
-    }
-    else return false
-  }
-  catch(err){
-    console.log(`Unexpected error: ${err}`)
-  }
-}
-
-function random4Digit(){
-  return Math.floor(1000 + Math.random() * 9000).toString()
-}
-
-
-app.use(express.urlencoded({extended: false}));
-app.use(express.json())
+// Middlewares
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+function hasProtocolHTTP(rawUrl) {
+  try {
+    const { protocol } = new URL(rawUrl);
+    return protocol === "http:" || protocol === "https:";
+  } catch {
+    return false;
+  }
+}
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use("/public", express.static(`${process.cwd()}/public`));
+
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", (req, res) => {
+  res.json({ greeting: "hello API" });
 });
 
-app.post('/api/shorturl', (req, res) => {
-  let url = req.body.url;
-  console.log(url)
-  if(checkUrl(url)){
-    console.log(url)
-    let shortUrl = random4Digit()
-    urls[shortUrl] = url
-    res.json({original_url: url, short_url: shortUrl})
+app.post("/api/shorturl", (req, res) => {
+  console.log(req.body);
+  const url = req.body.url;
+  // Validate protocol first
+  if (!hasProtocolHTTP(url)) {
+    return res.json({ error: "invalid url" });
   }
-  else{
-    console.log('Invalid URL')
-    res.json({error: 'invalid url'})
-  }
-})
 
-app.get('/api/shorturl/:shorturl', (req, res) => {
-  let shorturl = req.params.shorturl;
-  console.log(shorturl)
-  if(urls[shorturl]){
-    redirectUrl = urls[shorturl];
-    dns.lookup(redirectUrl, () => {
-      res.redirect(redirectUrl)
-    });
-  }
-  else{
-    res.json({
-      error: "No short URL found for the given input"
-    })
-  }
-})
+  dns.lookup(new URL(url).hostname, async (err, address) => {
+    if (err || !address) {
+      return res.json({ error: "invalid url" });
+    }
 
-app.listen(port, function() {
+    const urlCount = await urls.countDocuments();
+    const urlDoc = {
+      url,
+      short_url: urlCount,
+    };
+    const results = await urls.insertOne(urlDoc);
+    console.log(results);
+    res.json({ original_url: url, short_url: urlCount });
+  });
+});
+
+app.get("/api/shorturl/:shorturl", async (req, res) => {
+  const shorturl = req.params.shorturl;
+  const urlDoc = await urls.findOne({ short_url: +shorturl });
+  res.redirect(urlDoc.url);
+});
+
+app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
